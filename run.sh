2b01cee796f264485abc87e4a4252357d340c903
Seiya Sakaguchi <omprand.serbei@gmail.com>
2015-01-04 01:45:32 +0900

    メール配信用のスクリプトを最新状態に更新
    別リポジトリで進めていた変更を適応。
    詳細は省略。

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
-gc_type=`/home/seiya/.rbenv/shims/ruby /home/seiya/projects/gabAlert/main.rb`
-date=`date`
-message="今日のゴミ回収は${gc_type}です。"
+gc_type=`/home/seiya/.rbenv/shims/ruby /home/seiya/projects/gabAlert/today.rb`
+nil_msg='ごみ回収はない'
 
-echo "${date}${message}" | /usr/bin/mail -s "$gc_type" "omprand.serbei@gmail.com"
+if test "${gc_type}" != "${nil_msg}"
+then
+  date=`date`
+  message="今日のゴミ回収は${gc_type}です。"
+
+  echo "${date}${message}" | /usr/bin/mail -s "$gc_type" "omprand.serbei@gmail.com"
+fi
 
