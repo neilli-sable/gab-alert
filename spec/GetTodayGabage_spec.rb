a442ee0fcb9c0983c56c229f19f1212f05b45b89
坂口 聖弥 <seiya.sakaguchi@logosware.com>
2017-01-23 00:51:39 +0900

    テストコードを追加

--- spec/GetTodayGabage_spec.rb
+++ spec/GetTodayGabage_spec.rb
@@ -7,9 +7,45 @@ describe GabAlert::GetTodayGabage, "when Proceed" do
   end
 
   it "expect return 古紙・古布 (4, Tue)" do
-    time = Time.local(2014, 11,25, 0, 0, 0)
+    time = Time.local(2014, 11, 25, 0, 0, 0)
     Timecop.freeze(time)
     expect(@gabage.name).to eq("古紙・古布")
+  end
+
+  it "expect return 収集なし (1, Fry)" do
+    time = Time.local(2016, 1, 1, 0, 0, 0)
+    Timecop.freeze(time)
+    expect(@gabage.name).to eq("ごみ回収はない")
+  end
+
+  it "expect return 燃やせる (1, Sun)" do
+    time = Time.local(2016, 1, 3, 0, 0, 0)
+    Timecop.freeze(time)
+    expect(@gabage.name).to eq("燃えるごみ")
+  end
+
+  it "expect return ペットボトル (2, Wed)" do
+    time = Time.local(2017, 1, 11, 0, 0, 0)
+    Timecop.freeze(time)
+    expect(@gabage.name).to eq("ペットボトル")
+  end
+
+  it "expect return 収集なし (1, Mon)" do
+    time = Time.local(2017, 1, 2, 0, 0, 0)
+    Timecop.freeze(time)
+    expect(@gabage.name).to eq("ごみ回収はない")
+  end
+
+  it "expect return 燃やせる (1, Tue)" do
+    time = Time.local(2017, 1, 3, 0, 0, 0)
+    Timecop.freeze(time)
+    expect(@gabage.name).to eq("燃えるごみ")
+  end
+
+  it "expect return かん (4, Tue)" do
+    time = Time.local(2017, 1, 24, 0, 0, 0)
+    Timecop.freeze(time)
+    expect(@gabage.name).to eq("かん")
   end
 
   after do
